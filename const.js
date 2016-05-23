39bf3b718c93a92e42f4768fe67127178916b2fd
Shahzil Aftab <Shahzil Aftab>
2016-05-22 20:08:02 -0500

    Updated Const

--- const.js
+++ const.js
@@ -11,7 +11,7 @@ const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 if (!FB_VERIFY_TOKEN) {
-  FB_VERIFY_TOKEN = "just_do_it";
+  FB_VERIFY_TOKEN = "shazy792resumebot";
 }
 
 module.exports = {
