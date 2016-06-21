756cf200405da9d81711fca856b4985ff4f0964b
Shahzil Aftab <Shahzil Aftab>
2016-06-20 21:53:11 -0500

    Card Update

--- const.js
+++ const.js
@@ -7,7 +7,7 @@ if (!WIT_TOKEN) {
 }
 
 // Messenger API parameters
-const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+const FB_PAGE_TOKEN = C;
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 if (!FB_VERIFY_TOKEN) {
--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.listen(app.get('port'), function() {
       }
       if (event.postback) {
         let text = JSON.stringify(event.postback)
-        sendTextMessage(sender, "Postback received: Yolo"+text.substring(0, 200), token)
+        sendTextMessage(sender, "Recieved Post Back Call Payload= "+text, token)
         continue
       }
     }
